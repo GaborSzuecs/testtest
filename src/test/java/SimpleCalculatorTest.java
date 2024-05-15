3b6f813566f79356a759f739b88d04706284a676
GaborSzuecs <gabor.szuecs@tn.mischok.academy>
2024-05-15 08:25:38 +0200

    testest

---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import academy.mischok.SimpleCalculator;
+import org.junit.jupiter.api.Test;
+
+public class SimpleCalculatorTest {
+    final SimpleCalculator calculator = new SimpleCalculator();
+    @Test
+    public void testAdd() {
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        int result = calculator.multiply(3, 5);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testDivide() {
+        int result = calculator.divide(15, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void exceptionTesting() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                calculator.divide(1, 0));
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+
+    // Weitere Testfälle für subtract, multiply und divide hier einfügen
+}
+
